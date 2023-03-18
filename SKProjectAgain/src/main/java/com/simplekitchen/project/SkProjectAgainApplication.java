b177958bb75e2eb513773030c524f9fca844a7ba
serega <serega.135246@mail.ru>
2023-03-18 15:03:27 +0300

    Сборка данной версии в WAR - деплоится на WildFly сервер

--- SKProjectAgain/src/main/java/com/simplekitchen/project/SkProjectAgainApplication.java
+++ SKProjectAgain/src/main/java/com/simplekitchen/project/SkProjectAgainApplication.java
@@ -2,14 +2,21 @@ package com.simplekitchen.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * класс запуска приложения
  * @since 27.09.2022
  */
 @SpringBootApplication
-public class SkProjectAgainApplication {
+public class SkProjectAgainApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(SkProjectAgainApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SkProjectAgainApplication.class);
     }
 }
